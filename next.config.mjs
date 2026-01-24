aaa10710ec5fac1da824b7d3a348da41dc1bb675
itwmaccess <nstndo@gmail.com>
2026-01-24 13:28:02 +0300

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'base-archery-game.vercel.app',
-      },
-    ],
-  },
   webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
