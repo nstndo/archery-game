bbdb154e3bf1c441a026b76b4c94a8df9bb06af6
itwmaccess <nstndo@gmail.com>
2026-01-31 00:16:18 +0300

    Add Base app ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ const APP_URL = "https://base-archery-game.vercel.app";
 export const metadata: Metadata = {
   title: "Base Archery",
   description: "Compete in the Base Archery Tournament. Mint your score as NFT on Base.",
+  other: {
+    'base:app_id': '696eb06ac0ab25addaaaf6af'
+  },
   icons: {
     icon: 'https://base-archery-game.vercel.app/favicon.svg',
     shortcut: 'https://base-archery-game.vercel.app/favicon.svg',
